9b705fa215612e07f56fa9bffa75f8d28ac6c449
chourabi-dev <chourabitahers62@gmail.com>
2022-03-27 10:24:08 +0100

    latest

---
+++ day-8/chatbot/js/app.js
@@ -0,0 +1,6 @@
+var chtabot = document.getElementById('chtabot');
+var chatbotbar = document.getElementById('chatbotbar');
+
+chatbotbar.addEventListener('click',function(){
+    chtabot.classList="chatbot open";
+})
